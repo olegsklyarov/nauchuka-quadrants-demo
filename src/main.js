6f33582d39c272596b4abdef31857df87bc00bbf
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2020-04-18 21:45:56 +0300

    подготовка к лекции

---
+++ src/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function main() {
+  // eslint-disable-next-line no-console
+  console.log(window.mapCoordinates(600, 400, 640, 480));
+
+  // eslint-disable-next-line no-console
+  console.log(window.quadrants(100, 100));
+}());
